90f92554d861abe8a4435adecf226d67be61ce24
Romain CLEMENT <rc2301@orange.fr>
2023-01-31 16:54:26 +0100

    Initial commit

---
+++ src/main/java/fr/globostofo/baba_is_you_api/Main.java
@@ -0,0 +1,7 @@
+package fr.globostofo.baba_is_you_api;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/fr/globostofo/baba_is_you_api/TestMain.java
@@ -0,0 +1,14 @@
+package fr.globostofo.baba_is_you_api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+
+    @Test
+    void aTest() {
+        assertEquals(5, 2*2+1);
+    }
+
+}
