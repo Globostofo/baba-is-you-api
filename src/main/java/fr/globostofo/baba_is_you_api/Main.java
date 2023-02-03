d7b755dea8a2ec446ca9519475a4c93fbfad4404
Romain CLEMENT <rc2301@orange.fr>
2023-02-03 16:37:09 +0100

    Tried implement level 0

--- src/main/java/fr/globostofo/baba_is_you_api/Main.java
+++ src/main/java/fr/globostofo/baba_is_you_api/Main.java
@@ -1,7 +1,36 @@
 package fr.globostofo.baba_is_you_api;
 
+import fr.globostofo.baba_is_you_api.api.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Level lvl = new Level(18, 33);
+        lvl.addController(6, 11, EntityType.BABA);
+        lvl.addController(6, 19, EntityType.FLAG);
+        lvl.addController(14, 11, EntityType.WALL);
+        lvl.addController(14, 19, EntityType.ROCK);
+
+        lvl.addActor(10, 12, EntityType.BABA);
+        lvl.addActor(10, 20, EntityType.FLAG);
+        for (int c=11; c<=21; ++c) {
+            lvl.addActor(8, c, EntityType.WALL);
+            lvl.addActor(12, c, EntityType.WALL);
+        }
+        for (int r=9; r<=11; ++r)
+            lvl.addActor(r, 16, EntityType.ROCK);
+
+        lvl.addConnection(6, 12, ConnectionType.IS);
+        lvl.addConnection(6, 20, ConnectionType.IS);
+        lvl.addConnection(14, 12, ConnectionType.IS);
+        lvl.addConnection(14, 20, ConnectionType.IS);
+
+        lvl.addAttribute(6, 13, AttributeType.YOU);
+        lvl.addAttribute(6, 21, AttributeType.WIN);
+        lvl.addAttribute(14, 13, AttributeType.STOP);
+        lvl.addAttribute(14, 21, AttributeType.PUSH);
+
+        System.out.println(lvl);
     }
+
 }
--- src/main/java/fr/globostofo/baba_is_you_api/api/BlocksManager.java
+++ src/main/java/fr/globostofo/baba_is_you_api/api/BlocksManager.java
@@ -10,8 +10,11 @@ public class BlocksManager<K, V extends Block> {
     private final Map<K, List<V>> map = new HashMap<>();
 
     public void put(K key, V value) {
+        System.out.println(map);
         if (map.containsKey(key)) map.get(key).add(value);
-        else map.put(key, List.of(value));
+        else {
+            map.put(key, new ArrayList<>() {{add(value);}});
+        }
     }
 
     public List<V> getList(K key) {
